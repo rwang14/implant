be0249d36a12e7d5a0bc6ef8631545d50d56bad0
rwang14 <rhwang64@gmail.com>
2020-04-22 23:44:51 -0500

    demos

--- data-raw/demo.R
+++ data-raw/demo.R
@@ -1,13 +1,25 @@
-library(pkgload)
+#demo1
 data_new = read.csv(system.file("extdata", "data.csv",package = "implant", mustWork = TRUE))
-Y.na.mat = data_new[,-c(1:3)]
-tt = seq(from = 0, to = 44,by = 2)
-X = data_new[,c(2:3)]
+#The first column records the positions of the observations from the original dataset, can be ignored
+Y = data_new[,-c(1:3)]
 #This step is to factorize each factor
-X1 = as.factor(unlist(X[,1]))
-X2 = as.factor(unlist(X[,2]))
+X1 = as.factor(data_new$Genotype)
+X2 = as.factor(data_new$Block)
 X = data.frame(X1,X2)
-formula = toString("~ X[,1]+X[,2]")
-fit = fanova_mean(Y.na.mat, X, tt = tt, formula, K.int = 6, order = 4, d0 = 0, d1 = 2, d2 = 2, lower = -10, upper = 15)
+formula = "~ X[,1]+X[,2]"
+tt = seq(from = 0, to = 44,by = 2)
+fit = fanova_mean(Y.na.mat = Y, X = X, tt = tt, formula, K.int = 6, order = 4, lower = -10, upper = 15)
 fit$lambda
+fit$est_fun
+
+#demo2
+data_Xu = read.csv(system.file("extdata", "data_Xu.txt",package = "implant", 
+                               mustWork = TRUE), sep = "")
+X1 = as.factor(data_Xu$water)
+X2 = as.factor(data_Xu$genotype)
+X = data.frame(X1,X2)
+Y = data_Xu[,c(1:20)]
+fit = fanova_mean(Y.na.mat = Y, X = X,tt = c(0:15,17:20),
+                  formula = "~X[,1]+X[,2]+X[,1]:X[,2]"
+                  ,K.int = 6, order = 4,lower = -10, upper = 15)
 fit$est_fun
