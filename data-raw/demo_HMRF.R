4ccaad9f557935d21075baf084745df8e555af2b
rwang14 <rhwang64@gmail.com>
2020-04-29 10:14:50 -0500

    update

--- data-raw/demo_HMRF.R
+++ data-raw/demo_HMRF.R
@@ -1,19 +1,24 @@
 library(implant)
-library(imager)
-library(dplyr)
-orig1 = load.image(system.file("extdata", "image1.png",
-                               package = "implant", mustWork = TRUE))
-orig = resize(orig1, size_x = 500, size_y = 500, size_z = 1, size_c = 3)
+#library(imager)
+#library(dplyr)
+#orig1 = load.image(system.file("extdata", "image1.png", package = "implant", mustWork = TRUE))
+
+#orig = resize(orig1, size_x = 500, size_y = 500, size_z = 1, size_c = 3)
 #Define the response as relative green.
-I = orig[ , , 1, 2]/(orig[ , , 1, 1]+orig[ , , 1, 2]+orig[ , , 1, 3])
-Y = t(I)
-Z = cannyEdges(orig)
-Z = Z[ , , 1, 1]
-Z = t(Z)
+#I = orig[ , , 1, 2]/(orig[ , , 1, 1]+orig[ , , 1, 2]+orig[ , , 1, 3])
+#Y = t(I)
+orig = reaPNG(system.file("extdata", "reduced.png", package = "implant", mustWork = TRUE))
+#Define the response as relative green.
+Y = orig[ , , 2]/(orig[ , , 1]+orig[ , , 2]+orig[ , , 3])
+#Z is a matrix obtained by CannyEdge detector
+Z = read.csv(system.file("extdata", "Z.csv",
+                         package = "implant", mustWork = TRUE))
+##Note: Users can obtain Z using the package"imager" and the function CannyEdges( ) for different images
+#Z = cannyEdges(orig)
+#Z = t(Z[ , , 1, 1])
 #Take the initial label of EM algorithm using K-means
 X = image_kmeans(Y, k = 2)$X
-mu = image_kmeans(Y, k = 2)$mu
-sigma = image_kmeans(Y, k = 2)$sigma
+#Obtain the image produced by kmeans clustering
 output = matrix(as.numeric(X), nrow = nrow(X), ncol = ncol(X)) - 1
 #Run the HMRF Model. Note that this may take a lot of time ...
 img = HMRF(X, Y, Z, em_iter = 20, map_iter = 20, beta = 2,
