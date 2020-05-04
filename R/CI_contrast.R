b23ad74a555f4c3aed0eb5e96b0119c044152aa2
rwang14 <rhwang64@gmail.com>
2020-05-03 20:26:54 -0500

    Update CI_contrast.R

--- R/CI_contrast.R
+++ R/CI_contrast.R
@@ -23,17 +23,20 @@ CI_contrast = function(fit, j1, j2, alpha = 0.05){
   if(j1 == 1){
     C1 = 0
     trt = fit$est_fun[,j2]
+    print(sum(trt))
     C2 = C(K,nf,j2)
   }
   if (j2 == 1){
     C2 = 0
     trt = fit$est_fun[,j1]
     C1 = C(K,nf,j1)
+    print(sum(trt))
   }
   else{
     C1 = C(K,nf,j1)
     C2 = C(K,nf,j2)
-    trt = fit$est_fun[,j1]-fit$est_fun[,j2]
+    trt = fit$est_fun[,j1] - fit$est_fun[,j2]
+    print(sum(trt))
   }
   C = C1-C2
   }
